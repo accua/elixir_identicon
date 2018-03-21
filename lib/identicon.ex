5af3663935d51d9731c9dc555f3a607960d9411e
Andrew <andrewaccuardi@gmail.com>
2018-03-21 15:13:54 -0700

    initial commit including image struct and some functions for preparing the identicon data.

---
+++ lib/identicon.ex
@@ -0,0 +1,24 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex 
+    |> Enum.chunk(3)
+  end
+
+  def pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
+    %Identicon.Image{image | color: {red, green, blue}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
